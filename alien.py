f314fc5b6fc24563bc5c9060267828b113707f05
EddieYung <edwardquarshi@gmail.com>
2021-03-05 10:11:07 +0000

    aliens added

--- alien.py
+++ alien.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
-    """A class to rep a single alien in a fleet"""
+    """A class to represent a single alien in a fleet"""
     
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
@@ -22,8 +22,15 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
         
         
+    def check_edges(self):
+        """Return True if alien is at edge"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+        
+        
     def update(self):
         """Move the alien fleet to the right"""
-        self.x += self.settings.alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
         
